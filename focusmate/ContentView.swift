e1bb9cdeb9fd1510d3f8dc66ca39229d564bc535
Monsoud Zanaty <227215732+MonsoudZanaty@users.noreply.github.com>
2025-10-14 00:14:43 -0400

    Initial Commit

---
+++ focusmate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focusmate
+//
+//  Created by Monsoud Zanaty on 10/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focusmate/focusmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusmateApp.swift
+//  focusmate
+//
+//  Created by Monsoud Zanaty on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct focusmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
