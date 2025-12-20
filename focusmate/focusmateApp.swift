991f6e0d5fe27ea553c0de0f98c6d37d8c57e6a1
MonsoudZ <monsoud.zanaty@gmail.com>
2025-12-20 14:59:07 -0500

    cleaned up alot of files and build errors and tests that were not used

--- focusmate/focusmateApp.swift
+++
@@ -1,8 +0,0 @@
-//
-//  focusmateApp.swift
-//  focusmate
-//
-//  Created by Monsoud Zanaty on 10/14/25.
-//
-
-// Legacy app entry point removed in favor of FocusmateApp in Focusmate/App/FocusmateApp.swift
